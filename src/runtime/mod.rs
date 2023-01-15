dcd7f958f85542c74058815d767bfd5ab8d728d2
Jason Ozias <jason.g.ozias@gmail.com>
2023-01-14 23:40:13 -0500

    first attempt at issue body

--- src/runtime/mod.rs
+++ src/runtime/mod.rs
@@ -27,6 +27,7 @@ use reqwest::{
 use rustc_version::version_meta;
 use serde::{Deserialize, Serialize};
 use std::{
+    collections::BTreeMap,
     sync::mpsc::{channel, Receiver},
     thread::spawn,
 };
@@ -135,22 +136,43 @@ struct Resp {
 static APP_USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"),);
 
 lazy_static! {
-    static ref ID_REGEX: Regex = Regex::new(r#"ID: +(.*)"#).unwrap();
+    static ref CRATE_REGEX: Regex = Regex::new(r#"Crate: +(.*)"#).expect("Invalid CRATE_REGEX");
+    static ref VERSION_REGEX: Regex =
+        Regex::new(r#"Version: +(.*)"#).expect("Invalid VERSION_REGEX");
+    static ref WARNING_REGEX: Regex =
+        Regex::new(r#"Warning: +(.*)"#).expect("Invalid WARNING_REGEX");
+    static ref TITLE_REGEX: Regex = Regex::new(r#"Title: +(.*)"#).expect("Invalid TITLE_REGEX");
+    static ref DATE_REGEX: Regex = Regex::new(r#"Date: +(.*)"#).expect("Invalid DATE_REGEX");
+    static ref SOLUTION_REGEX: Regex =
+        Regex::new(r#"Solution: +(.*)"#).expect("Invalid SOLUTION_REGEX");
+    static ref ID_REGEX: Regex = Regex::new(r#"ID: +(RUSTSEC.*)"#).expect("Invalid ID_REGEX");
+    static ref URL_REGEX: Regex = Regex::new(r#"URL: +(https:.*)"#).expect("Invalid URL_REGEX");
+}
+
+#[allow(dead_code)]
+#[derive(Clone, Debug)]
+struct Rustsec {
+    id: String,
+    url: String,
+    krate: String,
+    version: String,
+    warning: String,
+    title: String,
+    date: String,
+    solution: String,
 }
 
 async fn create_issue(
     config: Config,
     stdout_buf: Vec<String>,
-    stderr_buf: Vec<String>,
+    _stderr_buf: Vec<String>,
 ) -> Result<Resp> {
     let token = config.token;
     let owner_repo = config.owner_repo;
     let stdout = stdout_buf.join("\n");
-    let id = if let Some(id_cap) = ID_REGEX.captures(&stdout) {
-        id_cap.get(1).map_or("Unknown ID", |m| m.as_str())
-    } else {
-        "Unknown ID"
-    };
+    let rustsec_map = parse(&stdout);
+    let title = generate_title(&rustsec_map);
+    let body = generate_body(&rustsec_map);
 
     let mut headers = HeaderMap::new();
     let _old = headers.insert(
@@ -167,12 +189,9 @@ async fn create_issue(
         .build()?;
 
     let url = format!("https://api.github.com/repos/{owner_repo}/issues");
-    info!("Posting to '{url}'");
-    info!("STDOUT: {}", stdout_buf.join("\n"));
-    info!("STDERR: {}", stderr_buf.join("\n"));
     let issue = Issue {
-        title: id.to_string(),
-        body: Some("This is another test issue".to_string()),
+        title,
+        body: Some(body),
         milestone: None,
         labels: None,
         assignees: None,
@@ -191,5 +210,130 @@ async fn create_issue(
         let body = res.bytes().await?;
         error!("{}", String::from_utf8_lossy(&body));
         Err(AuditCheckError::CreateIssue.into())
+    }
+}
+
+fn parse(output: &str) -> BTreeMap<String, Rustsec> {
+    let splits: Vec<&str> = output.split("\n\n").collect();
+
+    splits
+        .iter()
+        .map(parse_rustsec)
+        .map(|x| (x.id.clone(), x))
+        .collect()
+}
+
+fn parse_rustsec(rustsec_str: &&str) -> Rustsec {
+    let id = parse_caps(&ID_REGEX, rustsec_str, "No ID");
+    let url = parse_caps(&URL_REGEX, rustsec_str, "No URL");
+    let krate = parse_caps(&CRATE_REGEX, rustsec_str, "No Crate");
+    let version = parse_caps(&VERSION_REGEX, rustsec_str, "No Version");
+    let warning = parse_caps(&WARNING_REGEX, rustsec_str, "No Warning");
+    let title = parse_caps(&TITLE_REGEX, rustsec_str, "No Title");
+    let date = parse_caps(&DATE_REGEX, rustsec_str, "No Date");
+    let solution = parse_caps(&SOLUTION_REGEX, rustsec_str, "No Solution");
+
+    Rustsec {
+        id,
+        url,
+        krate,
+        version,
+        warning,
+        title,
+        date,
+        solution,
+    }
+}
+
+fn parse_caps(regex: &Regex, rustsec_str: &str, default: &str) -> String {
+    regex
+        .captures(rustsec_str)
+        .map_or_else(
+            || default,
+            |caps| caps.get(1).map_or(default, |m| m.as_str()),
+        )
+        .to_string()
+}
+
+fn generate_title(rustsec_map: &BTreeMap<String, Rustsec>) -> String {
+    rustsec_map.keys().fold(String::new(), |acc, key| {
+        if acc.is_empty() {
+            acc + key
+        } else {
+            acc + ", " + key
+        }
+    })
+}
+
+fn generate_body(rustsec_map: &BTreeMap<String, Rustsec>) -> String {
+    rustsec_map.iter().fold(String::new(), |acc, (k, v)| {
+        acc + &format!("# {}\n{}\n\n", k, v.url)
+    })
+}
+
+#[cfg(test)]
+mod test {
+    use super::{generate_title, parse};
+
+    const TEST_RUSTSEC: &str = r#"Crate:     aovec
+Version:   1.1.0
+Title:     Aovec<T> lacks bound on its Send and Sync traits allowing data races
+Date:      2020-12-10
+ID:        RUSTSEC-2020-0099
+URL:       https://rustsec.org/advisories/RUSTSEC-2020-0099
+Solution:  No fixed upgrade is available!
+Dependency tree:
+aovec 1.1.0
+└── audit-check-test 0.1.0
+
+Crate:     owning_ref
+Version:   0.3.3
+Title:     Multiple soundness issues in `owning_ref`
+Date:      2022-01-26
+ID:        RUSTSEC-2022-0040
+URL:       https://rustsec.org/advisories/RUSTSEC-2022-0040
+Solution:  No fixed upgrade is available!
+Dependency tree:
+owning_ref 0.3.3
+└── parking_lot 0.4.8
+    └── aovec 1.1.0
+        └── audit-check-test 0.1.0
+
+Crate:     anymap
+Version:   0.12.1
+Warning:   unmaintained
+Title:     anymap is unmaintained.
+Date:      2021-05-07
+ID:        RUSTSEC-2021-0065
+URL:       https://rustsec.org/advisories/RUSTSEC-2021-0065
+Dependency tree:
+anymap 0.12.1
+└── audit-check-test 0.1.0
+
+Crate:     smallvec
+Version:   0.4.5
+Warning:   unsound
+Title:     smallvec creates uninitialized value of any type
+Date:      2018-09-25
+ID:        RUSTSEC-2018-0018
+URL:       https://rustsec.org/advisories/RUSTSEC-2018-0018
+Dependency tree:
+smallvec 0.4.5
+└── aovec 1.1.0
+    └── audit-check-test 0.1.0
+"#;
+
+    #[test]
+    fn parse_works() {
+        assert_eq!(4, parse(TEST_RUSTSEC).len());
+    }
+
+    #[test]
+    fn generate_title_works() {
+        let rustsec_map = parse(TEST_RUSTSEC);
+        assert_eq!(
+            "RUSTSEC-2018-0018, RUSTSEC-2020-0099, RUSTSEC-2021-0065, RUSTSEC-2022-0040",
+            generate_title(&rustsec_map)
+        );
     }
 }
